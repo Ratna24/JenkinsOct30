a7c02fe804d01772d66ae5a89321d010c0c0869a
Kalvakolanu <ratnak@afms.net>
2018-10-31 12:02:18 +0530

    Adding First Sonarqube Project

---
+++ JenkinsSample/src/main/java/ProjectJenkins/JenkinsSample/TeamLead.java
@@ -0,0 +1,12 @@
+package ProjectJenkins.JenkinsSample;
+
+public class TeamLead extends Employee{
+	private int numberOfProjectsManaged;
+
+	public int getNumberOfProjectsManaged() {
+		return numberOfProjectsManaged;
+	}
+	public void setNumberOfProjectsManaged(int numberOfProjectsManaged) {
+		this.numberOfProjectsManaged = numberOfProjectsManaged;
+	}	
+}
